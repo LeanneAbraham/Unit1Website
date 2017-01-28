215afab673455bed85c67e17505a307c14d33402
LeanneAbraham <abraham.leanne@gmail.com>
2017-01-28 17:13:14 -0600

    Unit-1
    Module 2 activity

---
+++ js/Ex2.3.js
@@ -0,0 +1,84 @@
+// //execute function once page loads
+// function initialize(){
+//     cities();
+// };
+// function cities(){
+//   var citiesObj = {
+//     'Madison': 233209,
+//     'Milwaukee':594833,
+//     'Green Bay':104057,
+//     'Superior' :27244
+//   };
+//
+//   //var mydiv = $('#mydiv');
+//   $('#mydiv').append('<table><tr id="headerRow">');
+//   $('#headerRow').append("<th>City</th><th>Population</th>");
+//   for (cityKey in citiesObj) {
+//     var rowHTML = "<tr><td>"+ cityKey + "</td><td>"+citiesObj[cityKey]+"</td></tr>";
+//     $("table").append(rowHTML);
+//
+//   }
+//
+//
+//
+// };
+// //   //array to define city names
+// //     var cities = [
+// //         'Madison',
+// //         'Milwaukee',
+// //         'Green Bay',
+// //         'Superior'
+// //     ];
+// //
+// // console.log(cities);
+// //
+// //     var population = [
+// //       //array to define city populations
+// //         233209,
+// //         594833,
+// //         104057,
+// //         27244
+// //     ];
+//
+//
+//     // //creates html table element
+//     // var table = document.createElement("table");
+//     // //creates html row element
+//     // var headerRow = document.createElement("tr");
+//     // //creates html header cell element
+//     // var cityHeader = document.createElement("th");
+//     // cityHeader.innerHTML = "City";
+//     // headerRow.appendChild(cityHeader);
+//     // //creates header cell for population
+//     // var popHeader = document.createElement("th");
+//     // popHeader.innerHTML = "Population";
+//     // headerRow.appendChild(popHeader);
+//     // //adds header row to table
+//     // table.appendChild(headerRow);
+//     // //creates a varabile called 'i' and setting it to zero, annd then saying what to say each time through the array as long as it is less than the cities array
+//     // for (var cityKey in citiesObj[city]){
+//     //
+//     //   //creates table row
+//     //     var tr = document.createElement("tr");
+//     //   //creates table cell
+//     //     var city = document.createElement("td");
+//     //     city.innerHTML = cityKey;
+//     //     tr.appendChild(city);
+//     //     console.log(cities,i,cities);
+//     //     var pop = document.createElement("td");
+//     //     pop.innerHTML = population[i];
+//     //     tr.appendChild(pop);
+//     //     //add row to table
+//     //     table.appendChild(tr);
+//     // };
+//
+//     // var mydiv = document.getElementById("mydiv");
+//     // mydiv.appendChild(table);
+// // };
+//
+// //this is one type of comment
+// /*
+// this is another type of comment
+// */
+//
+// window.onload = initialize();
